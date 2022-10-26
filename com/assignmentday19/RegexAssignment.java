48b61bdefbb7fe3300ae815372a4baa58cebb44d
Shiv25z <shivamzagade25899@gmail.com>
2022-10-26 14:34:49 +0530

    Added: User FirstName Validation Method

--- com/assignmentday19/RegexAssignment.java
+++ com/assignmentday19/RegexAssignment.java
@@ -1,7 +1,11 @@
 package com.assignmentday19;
 
+
 public class RegexAssignment {
     public static void main(String[] args) {
         System.out.println("------------------- Pattern Matching Assignment -------------------");
+        ValidateUserInfo validateUserInfo = new ValidateUserInfo();
+        validateUserInfo.checkFirstName();// UC1
     }
+
 }
---
+++ com/assignmentday19/ValidateUserInfo.java
@@ -0,0 +1,20 @@
+package com.assignmentday19;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class ValidateUserInfo {
+    Scanner sc = new Scanner(System.in);
+    public void checkFirstName(){
+        System.out.println("Enter FirstName: ");
+        String s = sc.nextLine();
+        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}",s)){
+            System.out.println("FirstName Validated");
+        }
+        else{
+            System.out.println("Invalid Input!");
+        }
+
+    }
+}
